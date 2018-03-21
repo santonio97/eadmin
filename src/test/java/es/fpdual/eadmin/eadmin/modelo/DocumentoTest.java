a9bf932b7ac3850e5d149b49072f256df4c1a68e
formacion <formacion@192.168.86.128>
2018-03-21 11:08:24 +0100

    klnzxdvklcdzsklñ

--- src/test/java/es/fpdual/eadmin/eadmin/modelo/DocumentoTest.java
+++ src/test/java/es/fpdual/eadmin/eadmin/modelo/DocumentoTest.java
@@ -52,4 +52,9 @@ public class DocumentoTest {
 		final Boolean resultado = documento.equals(new Date());
 		assertTrue(resultado);
 	}
+	
+	@Test
+	public void pruebaDeltoString() {
+		assertEquals("Documento con codigo" + documento.getCodigo(),documento.toString());
+	}
 }
