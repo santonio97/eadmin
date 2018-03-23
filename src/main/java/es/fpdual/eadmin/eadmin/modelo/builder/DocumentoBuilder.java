4eed9fd883f05469c09e1dd37c5de82e363a1313
formacion <formacion@mvformacion.localdomain>
2018-03-23 11:06:57 +0100

    23/03/2018 11:06

---
+++ src/main/java/es/fpdual/eadmin/eadmin/modelo/builder/DocumentoBuilder.java
@@ -0,0 +1,59 @@
+package es.fpdual.eadmin.eadmin.modelo.builder;
+
+import java.util.Date;
+import es.fpdual.eadmin.eadmin.modelo.Documento;
+import es.fpdual.eadmin.eadmin.modelo.EstadoDocumento;
+
+public class DocumentoBuilder {
+
+	protected Integer codigo;
+	protected String nombre;
+	protected Date fechaCreacion;
+	protected Boolean publico;
+	protected EstadoDocumento estado;
+
+	public Documento construir() {
+
+		return new Documento(codigo, nombre, fechaCreacion, publico, estado);
+	}
+
+	public DocumentoBuilder conCodigo(Integer codigo) {
+
+		this.codigo = codigo;
+		return this;
+	}
+
+	public DocumentoBuilder conNombre(String nombre) {
+
+		this.nombre = nombre;
+		return this;
+	}
+
+	public DocumentoBuilder conFechaCreacion(Date fechaCreacion) {
+
+		this.fechaCreacion = fechaCreacion;
+		return this;
+	}
+
+	public DocumentoBuilder conPublico(boolean publico) {
+
+		this.publico = publico;
+		return this;
+	}
+
+	public DocumentoBuilder conEstado(EstadoDocumento estadoDocumento) {
+
+		this.estado = estadoDocumento;
+		return this;
+	}
+
+	public DocumentoBuilder clonar(Documento documento) {
+
+		this.codigo = documento.getCodigo();
+		this.nombre = documento.getNombre();
+		this.fechaCreacion = documento.getFechaCreacion();
+		this.publico = documento.getPublico();
+		this.estado = documento.getEstado();
+		return this;
+	}
+}
