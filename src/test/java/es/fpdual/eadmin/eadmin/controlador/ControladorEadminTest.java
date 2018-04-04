13884404fe4b916b493922a8d48e7db6e630c4b8
formacion <formacion@192.168.86.128>
2018-04-04 11:50:47 +0200

    11:50

---
+++ src/test/java/es/fpdual/eadmin/eadmin/controlador/ControladorEadminTest.java
@@ -0,0 +1,38 @@
+package es.fpdual.eadmin.eadmin.controlador;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import es.fpdual.eadmin.eadmin.modelo.Documento;
+import es.fpdual.eadmin.eadmin.servicio.ServicioDocumento;
+
+public class ControladorEadminTest {
+
+	private ControladorEadmin controlador;
+	
+	private final ServicioDocumento servicio = mock(ServicioDocumento.class);
+			
+	@Before
+	public void inicializarEnCadaTest() {
+		this.controlador = new ControladorEadmin(servicio);
+		
+	}
+	
+	@Test
+	public void deberiaDevolverTodosLosDocumentos() {
+		
+		final List<Documento> documentos = new ArrayList<>();
+		when(this.servicio.obtenerTodosLosDocumentos()).thenReturn(documentos);
+		
+		List<Documento> resultado = this.controlador.getTodosDocumentos().getBody();
+		
+		assertSame(resultado, documentos);
+	}
+	
+}
