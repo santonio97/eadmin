156005bd63181b7ad83205caf54dd789a7aa78ac
formacion <formacion@192.168.86.128>
2018-04-13 13:55:32 +0200

    13:55

--- src/main/java/es/fpdual/eadmin/eadmin/mapper/handler/EstadoDocumentoTypeHandler.java
+++ src/main/java/es/fpdual/eadmin/eadmin/mapper/handler/EstadoDocumentoTypeHandler.java
@@ -6,29 +6,28 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.apache.ibatis.type.*;
-import org.apache.poi.ss.formula.functions.T;
-
 import es.fpdual.eadmin.eadmin.modelo.EstadoDocumento;
 
 public abstract class EstadoDocumentoTypeHandler implements TypeHandler<EstadoDocumento> {
 
-	public void setParameter(PreparedStatement ps, int i, T parameter, JdbcType jdbcType) throws SQLException {
-	
-		
+	public void setParameter(PreparedStatement ps, int columnIndex, EstadoDocumento estadoDocumento, JdbcType jdbcType)
+			throws SQLException {
+
+		ps.setInt(columnIndex, estadoDocumento.getCodigo());
 	}
 
 	public EstadoDocumento getResult(ResultSet rs, String columnName) throws SQLException {
-		return null;
-	
+
+		return EstadoDocumento.obtenerPorCodigo(rs.getInt(columnName));
 	}
 
 	public EstadoDocumento getResult(ResultSet rs, int columnIndex) throws SQLException {
-		return null;
-	
+		return EstadoDocumento.obtenerPorCodigo(rs.getInt(columnIndex));
+
 	}
 
 	public EstadoDocumento getResult(CallableStatement cs, int columnIndex) throws SQLException {
-		return null;
-	
+		return EstadoDocumento.obtenerPorCodigo(cs.getInt(columnIndex));
+
 	}
 }
